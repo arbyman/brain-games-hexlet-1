340af8b10d915c31e107b2ed2e6142d11d4a795b
Sergey Vanin <bardelorean@yandex.ru>
2019-03-23 12:20:13 +0300

    edit progr

--- src/games/progression.js
+++ src/games/progression.js
@@ -14,10 +14,10 @@ export default () => {
     const start = getRandomNumber(1, 100);
     const step = getRandomNumber(2, 7);
     const length = getRandomNumber(8, 12);
-    const indexOfRemovedElement = getRandomNumber(0, length - 1);
+    const indexOfHiddenElement = getRandomNumber(0, length - 1);
     const progression = createProgression(start, step, length);
-    const removedElement = progression.splice(indexOfRemovedElement, 1, '..');
-    const correctAnswer = removedElement.toString();
+    const correctAnswer = progression[indexOfHiddenElement].toString();
+    progression[indexOfHiddenElement] = '..';
     const question = progression.join(' ');
     return [question, correctAnswer];
   };
